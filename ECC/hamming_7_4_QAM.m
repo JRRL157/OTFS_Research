315272178a38acca6b5f4062aae4789ead78dde1
JRRL157 <jrrlino157@gmail.com>
2025-09-04 22:14:16 -0300

    [ECC] Hamming (7,4) Working with 4QAM

--- ECC/hamming_7_4_QAM.m
+++ ECC/hamming_7_4_QAM.m
@@ -3,21 +3,33 @@ clear;
 
 N = 4;
 M = 128;
+mod_order = 4;
 
-m = floor(log2(N*M + 1));
+% OTFS Frame Length (Decimal)
+L = N * M;
 
-disp(m);
+% OTFS Frame Length (Binary)
+bL = L * log2(mod_order); 
+
+% Codeword length (n) is 2^m -1, n = 2^m -1
+% Message length (k) is n - m, k = n - m
+m = floor(log2(bL + 1));
+
+%disp(m);
 [H, G, n, k] = hammgen(m);
 
-disp(G);
-disp(H);
+%disp(G);
+%disp(H);
 
-m = randi([0 1], [k 1]);
+msg = randi([0 1], [k 1]);
 
-encoded = mod(G.' * m, 2);
+disp('Message: ')
+disp(num2str(msg.'));
 
-disp('Encoded: ');
-disp(num2str(encoded));
+encoded = mod(G.' * msg, 2);
+
+%disp('Encoded: ');
+%disp(num2str(encoded));
 
 % Adding one error
 i = 100;
@@ -25,9 +37,9 @@ encoded(i) = mod(encoded(i) + 1, 2);
 
 syndrome = mod(H * encoded, 2);
 
-disp('Syndrome: ');
-disp(syndrome);
-disp(H);
+%disp('Syndrome: ');
+%disp(syndrome);
+%disp(H);
 
 % Find the error position by comparing syndrome with columns of H
 error_pos = find(ismember(H.', syndrome.', 'rows'));
@@ -39,9 +51,9 @@ end
 
 disp('Decoded: ');
 decoded = encoded(end-k+1:end);
-disp(num2str(decoded));
+disp(num2str(decoded.'));
 
-if isequal(m, decoded)
+if isequal(msg, decoded)
     disp('Decoded message matches the original message.');
 else
     disp('Decoded message does NOT match the original message.');
