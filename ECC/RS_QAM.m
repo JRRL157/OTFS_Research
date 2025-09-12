4d7f15146b2bae3d02b4ee4791e4f75fc1c1a7f5
JRRL157 <jrrlino157@gmail.com>
2025-09-12 09:52:46 -0300

    [ECC] RS QAM
    Did some adjusments to fit in N*M OTFS Frame

--- ECC/RS_QAM.m
+++ ECC/RS_QAM.m
@@ -2,9 +2,9 @@ clear;
 clc;
 
 N = 4;
-M = 128;
+M = 32;
 
-mod_order = 256;
+mod_order = 16;
 bps = log2(mod_order); %Bits per symbol
 
 % OTFS Frame Length (Decimal)
@@ -51,8 +51,9 @@ for i=1:num_blocks
   tx_bits = [tx_bits; msg];
   encoded_bits = [encoded_bits; enc_msg];
 end
-% Encoded bits
-% encoded_bits = rsEncoder(random_bits_msg);
+% Encoded bits padding
+bits2pad = L * bps - length(encoded_bits);
+encoded_bits = [encoded_bits; zeros(bits2pad, 1)];
 
 tx_symbols = qammod(encoded_bits, mod_order, InputType='bit');
 
@@ -68,8 +69,11 @@ rx_symbols = awgn(tx_symbols,SNR);
 % Demodulation
 demod_bits = qamdemod(rx_symbols, mod_order, OutputType='bit'); 
 
+% Unpad bits
+unpadded_bits = demod_bits(1: L * bps - bits2pad);
+
 % RS Decoding
-rx_bits = rsDecoder(demod_bits);
+rx_bits = rsDecoder(unpadded_bits);
 
 if isequal(tx_bits, rx_bits)
     disp('[SUCCESS] Decoded message matches the original message.');
