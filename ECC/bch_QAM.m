c6754a7c59ac15b54c5cc7f0d358e7bb179d8e1d
JRRL157 <jrrlino157@gmail.com>
2025-09-26 13:40:16 -0300

    [ECC] BCH QAM script updated

--- ECC/bch_QAM.m
+++ ECC/bch_QAM.m
@@ -2,7 +2,7 @@ clear;
 clc;
 
 N = 4;
-M = 4;
+M = 128;
 
 mod_order = 4;
 bps = log2(mod_order); %Bits per symbol
@@ -36,14 +36,11 @@ num_src_bits = num_blocks * k;
 
 % Random message bits which will be sent
 random_bits_msg = randi([0 1], num_src_bits, 1);
-% random_bits_msg = gf(randi([0 1], num_src_bits, k));
 
 % Encoded bits
 encoded_bits = [];
 for i = 1:num_blocks
 	message_block = random_bits_msg((i-1)*k + 1 : i*k);
-	% cword = mod(G.' * message_block, 2);
-  % cword = bchenc(message_block, n, k);
   cword = enc(message_block);
 	encoded_bits = [encoded_bits; cword];
 end
@@ -67,13 +64,6 @@ demod_bits = qamdemod(rx_symbols, mod_order, OutputType='bit');
 decoded_bits = [];
 for i = 1:num_blocks
 	rec_cword =  demod_bits((i-1)*n + 1: i*n);
-	% syndrome = mod(H * rec_cword, 2);
-	% error_idx = find(ismember(H.', syndrome.', 'rows'));
-	% if ~isempty(error_idx)
-	% 	rec_cword(error_idx) =  mod(rec_cword(error_idx) + 1, 2);
-	% end
-	% msg_block = rec_cword(end-k+1:end);
-  % msg_block = bchdec(rec_cword, n, k);
   msg_block = dec(rec_cword);
 	decoded_bits = [decoded_bits; msg_block];
 end
