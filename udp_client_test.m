d1d157e48331f2e1b6983167224db884c7139786
JRRL157 <jrrlino157@gmail.com>
2025-10-03 13:02:06 -0300

    [ECC] LDPC Udp Client Test updated
    Did some tests with high cardinality Modulations (256QAM, 1024QAM, 2048QAM)

--- udp_client_test.m
+++ udp_client_test.m
@@ -5,9 +5,10 @@ server_ip = '127.0.0.1';
 server_port = 5016;
 
 cmd_code = 0;
-M = 32;
+M = 44;
 CR = 1/2;
 K = ceil(M * CR);
+mod_order = 2048;
 
 data = [cmd_code, M, CR];
 
@@ -24,22 +25,21 @@ disp("Original: " + num2str(random_bits_msg));
 disp("Encoded: " + num2str(Y));
 
 cmd_code = 2;
-qam_symbols = qammod(Y.', 4, 'InputType', 'bit', 'UnitAveragePower', true);
+qam_symbols = qammod(Y.', mod_order, 'InputType', 'bit', 'UnitAveragePower', true);
 
-snr_db = 20;
+snr_db = 30;
 snr_linear = 10^(snr_db/10);
 noise_var = 1/snr_linear;
 noise = sqrt(noise_var/2) * randn(size(qam_symbols));
 
 rx_symbols = qam_symbols + noise;
 
-demod_llrs = qamdemod(rx_symbols, 4, ...
+demod_llrs = qamdemod(rx_symbols, mod_order, ...
     'OutputType', 'approxllr', ...
     'NoiseVariance', noise_var, ...
     'UnitAveragePower', true);
 
-
-data = [cmd_code, 10, length(demod_llrs), demod_llrs.'];
+data = [cmd_code, 100, length(demod_llrs), demod_llrs.'];
 
 Z = NR_LDPC_client(server_ip, server_port, data, K);
 
