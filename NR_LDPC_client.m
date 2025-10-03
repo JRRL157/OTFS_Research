727ad6d9bf503013ff8d833d646bda20bbd6f6ab
JRRL157 <jrrlino157@gmail.com>
2025-10-03 08:05:37 -0300

    [ECC] NR_LDPC UDP Client Updates

--- NR_LDPC_client.m
+++ NR_LDPC_client.m
@@ -1,12 +1,12 @@
 function Y = NR_LDPC_client(server_ip, server_port, data, length_data)
   uByte = udpport("byte", "Timeout", 10); % Increase timeout to 10 seconds
-
+  
   write(uByte, data, "double", server_ip, server_port);
 
   try
-    Y = read(uByte, length_data -1, "uint8");
-  catch ME
-    warning("Error during data transmission: %s", MEx.message);
+    Y = read(uByte, length_data, "uint8");
+  catch MException
+    warning("Error during data transmission!");
     Y = [];
   end
 
